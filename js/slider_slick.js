919a9d71cf73f56e9bf8c06536a01d644c284752
Diana-Diana-dev <77507565+Diana-Diana-dev@users.noreply.github.com>
2021-09-20 11:17:53 +0300

    feat: add folder JS

---
+++ js/slider_slick.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	$('.slider').slick({
+	 	  dots:false,
+	 	  slidesToShow:3,
+	 	  speed:1000,
+	 	  easing:'ease',
+	 	  infinite:true,
+	 	  autoplay:true,
+	 	  autoplaySpeed:2000,
+	 	  touchThreshold:10,
+	 	  waitForAnimate:false,
+	 	  centerMode:false,
+	 	  variableWidth:false,
+         responsive: [
+        {
+     		 breakpoint: 1024,
+     		 settings: {
+          		slidesToShow: 2,
+          		 arrows: false
+          	}
+         },
+         {
+         	breakpoint: 768,
+     		 settings: {
+          		slidesToShow: 1,
+              arrows: false
+         }
+     }
+        ]
+	 });
+});
